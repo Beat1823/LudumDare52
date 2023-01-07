058708d26220c46727fe35344da13c2be6d49188
Mikhai <romeobear.raf@gmail.com>
2023-01-08 00:14:51 +0100

    Added smoothed movemen, and a lot of settings for character

--- Game/NPCS/NeutralNpc.gd
+++ Game/NPCS/NeutralNpc.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-signal absorbed
+signal absorbed (linkToSelf)
 
 func _ready():
 	$AnimatedSprite.playing = true
@@ -10,4 +10,6 @@ func _on_VisibilityNotifier2D_viewport_exited(viewport):
 	queue_free()
 
 func _on_enemy_body_entered(body_rid, body, body_shape_index, local_shape_index):
-	emit_signal("absorbed") # Should check if the body is the player/thing we want to use to absorb.
+	if body_rid.is_in_group("MainCharacterHarvester"):
+		emit_signal("absorbed",self) # should check if the body is the player/thing we want to use to absorb.
+
