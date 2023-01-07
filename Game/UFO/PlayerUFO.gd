fee594d46f067feb3d916b8b4b3b6b6f11cf9424
Mikhai <romeobear.raf@gmail.com>
2023-01-07 16:36:49 +0100

    Added Tracktor beam

--- Game/UFO/PlayerUFO.gd
+++ Game/UFO/PlayerUFO.gd
@@ -4,8 +4,12 @@ var thrust = Vector2(0, -500)
 var reverse_thrust = Vector2(0, 250)
 var torque = 60000
 func _ready():
-	pass
-
+	SetTractorBeam (false)
+	
+func SetTractorBeam (IsON):
+	$TracktorBeamArea/TrackBeamCollision.disabled = !IsON
+	$HarvestArea/CollisionShape2D.disabled = !IsON
+	$TracktorBeamArea/AnimatedSprite.visible = IsON
 func _integrate_forces(state):
 	if Input.is_action_pressed("Up"):
 		set_applied_force(thrust.rotated(rotation))
@@ -19,3 +23,13 @@ func _integrate_forces(state):
 	if Input.is_action_pressed("Left"):
 		rotation_dir -= .1
 	set_applied_torque(rotation_dir * torque)
+func _process(delta):
+	if Input.is_action_pressed("TractorBeam"):
+		SetTractorBeam (true)
+	else:
+		SetTractorBeam (false)
+		
+
+func _on_HarvestArea_body_entered(body):
+	pass
+	
