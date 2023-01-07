08858aafbdba730c9f5be0db11ca3a14f5709924
Cameron <beat1823@gmail.com>
2023-01-07 22:14:56 +0100

    Add emit for something entering enemy body.

--- Game/NPCS/NeutralNpc.gd
+++ Game/NPCS/NeutralNpc.gd
@@ -1,8 +1,13 @@
 extends RigidBody2D
 
+signal absorbed
+
 func _ready():
 	$AnimatedSprite.playing = true
 	
 
 func _on_VisibilityNotifier2D_viewport_exited(viewport):
 	queue_free()
+
+func _on_enemy_body_entered(body_rid, body, body_shape_index, local_shape_index):
+	emit_signal("absorbed") # Should check if the body is the player/thing we want to use to absorb.
