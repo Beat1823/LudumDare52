3a05272f64b6bcf4c937436641ae32883c5899d3
Cameron <beat1823@gmail.com>
2023-01-07 21:50:22 +0100

    Add chicken, cow scene. Changes to main scene.

--- Game/MainScene.gd
+++ Game/MainScene.gd
@@ -1,11 +1,40 @@
 extends Node2D
+export var SpawnLocations = []
+export var Npcs = [preload("res://Game/NPCS/Cow/Cow.tscn"), preload("res://Game/NPCS/Chicken/Chicken.tscn")]
 
+var score = 0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize()
+	newgame()
+	
+func gameover():
+	$ScoreTimer.stop()
+	$SpawnTimer.stop()
+
+func newgame():
+	score = 0
+	$PlayerUFO.position = $StartPos.position
+	$StartWaitTimer.start()
+	
+func addscore(points):
+	score += points
+	
+func _on_StartWaitTimer_timeout():
+	$ScoreTimer.start()
+	$SpawnTimer.start()
+
+func _on_ScoreTimer_timeout():
+	score += 1
+
+func _on_SpawnTimer_timeout():
+	var mob = Npcs[randi() % Npcs.size()].instance()
+	mob.position = SpawnLocations[randi() % SpawnLocations.size()]
+
+
+	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+	
+	mob.linear_velocity = velocity
+	mob.get_node("AnimatedSprite").flip_h = mob.linear_velocity.x < 0
+
+	add_child(mob)
--- Game/NPCS/NeutralNpc.gd
+++ Game/NPCS/NeutralNpc.gd
@@ -2,7 +2,6 @@ extends RigidBody2D
 
 func _ready():
 	$AnimatedSprite.playing = true
-	$AnimatedSprite.animation = "Cow"
 	
 
 func _on_VisibilityNotifier2D_viewport_exited(viewport):
