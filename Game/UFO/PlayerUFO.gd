0aeb23dde6b01716f998c5db30adf9638d31d847
Mikhai <romeobear.raf@gmail.com>
2023-01-07 16:36:49 +0100

    Added Tracktor beam

--- Game/UFO/PlayerUFO.gd
+++ Game/UFO/PlayerUFO.gd
@@ -2,10 +2,15 @@ extends RigidBody2D
 
 var thrust = Vector2(0, -500)
 var reverse_thrust = Vector2(0, 250)
-var torque = 60000
+var torque = 6000
+var stabilize_factor = 0.2
 func _ready():
-	pass
-
+	SetTractorBeam (false)
+	
+func SetTractorBeam (IsON):
+	$TracktorBeamArea/TrackBeamCollision.disabled = !IsON
+	$HarvestArea/CollisionShape2D.disabled = !IsON
+	$TracktorBeamArea/AnimatedSprite.visible = IsON
 func _integrate_forces(state):
 	if Input.is_action_pressed("Up"):
 		set_applied_force(thrust.rotated(rotation))
@@ -15,7 +20,26 @@ func _integrate_forces(state):
 		set_applied_force(Vector2())
 	var rotation_dir = 0
 	if Input.is_action_pressed("Right"):
-		rotation_dir += .1
+		rotation_dir += 1
 	if Input.is_action_pressed("Left"):
-		rotation_dir -= .1
-	set_applied_torque(rotation_dir * torque)
+		rotation_dir -= 1
+	print (rotation_dir)
+	if rotation_dir == 0:
+		StabilizeUFO()
+	else:
+		set_applied_torque(rotation_dir * torque)
+	
+func _process(delta):
+	if Input.is_action_pressed("TractorBeam"):
+		SetTractorBeam (true)
+	else:
+		SetTractorBeam (false)
+		
+
+func StabilizeUFO():
+	var stabilize_torque = -1*sign(rotation_degrees) * torque * stabilize_factor
+	set_applied_torque (stabilize_torque)
+	print (stabilize_torque)
+func _on_HarvestArea_body_entered(body):
+	pass
+	
