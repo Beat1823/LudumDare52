beaf5b924b7cd68db28f5869350783d2748dbd47
Mikhai <romeobear.raf@gmail.com>
2023-01-08 00:14:51 +0100

    Added smoothed movemen, and a lot of settings for character

--- Game/UFO/PlayerUFO.gd
+++ Game/UFO/PlayerUFO.gd
@@ -14,12 +14,31 @@ var collision_bounce = 0.2
 var previous_linear_velocity : Vector2 = Vector2.ZERO
 
 # Movement params
-var thrust = Vector2(0, -500)
-var reverse_thrust = Vector2(0, 250)
+export var thrustForwardForce= 500.0
+export var thrustReverseForce = 250
+var thrust = Vector2(0, -1*thrustForwardForce)
+var reverse_thrust = Vector2(0,thrustReverseForce)
 
-var torque = 6000
-var stabilize_torqueForce = 3000
+var forward_input_level = 0
+export var forward_input_rate_up = 1.0
+export var forward_input_rate_down = 3.0
+export (Curve) var ForwardInputCurve
+
+var reverse_input_level = 0
+export var reverse_input_rate_up = 0.5
+export var reverse_input_rate_down = 3.0
+export (Curve) var ReverseInputCurve
+
+var turn_input_level = 0
+var turn_input_rate_up = 100
+var turn_input_rate_down = 100
+
+export (Curve) var TurnInputCurve
+
+export var torque = 6000.0
+export var stabilize_torqueForce = 3000.0
 var shouldStabilize = false
+export var StabilizeDownAngleThershold = 90.0
 export (Curve) var stabilize_curve
 export(float) var StabilizeDelaySeconds
 
@@ -37,52 +56,82 @@ func SetTractorBeam (IsON):
 
 
 func _integrate_forces(state):
-	var finalforce = Vector2()
+	#Process Collision
+	var collision_force = Vector2()
 	#calculate impact if any
-	collision_force = Vector2.ZERO
+	var collision_impact = Vector2.ZERO
 	if state.get_contact_count() > 0:
 		var dv : Vector2 = state.linear_velocity - previous_linear_velocity
-		collision_force = dv / (state.inverse_mass * state.step)
-		print(collision_force)
+		collision_impact= dv / (state.inverse_mass * state.step)
+		print(collision_impact)
 		if (!$CollisionShape2D/HitParticles.emitting):
 			$CollisionShape2D/HitParticles.restart()
-		ReduceEnergy(collision_force.length()*ImpactEnergyModifier)
+		ReduceEnergy(collision_impact.length()*ImpactEnergyModifier)
 	previous_linear_velocity = state.linear_velocity
 	
-	if collision_force != Vector2.ZERO:
-		finalforce = collision_force*collision_bounce
+	if collision_impact != Vector2.ZERO:
+		collision_force = collision_impact*collision_bounce
+	var input_force = ProcessInputForce(state.step)
+	var input_torque= ProcessInputRotation(state)
+	
+	set_applied_force(collision_force+input_force)
+	set_applied_torque(input_torque)
+	
+	
+
+func StabilizeUFO(isStab):
+	if (!isStab):
+		return 0
+	
+	var t = abs(rotation_degrees/180)
+	var modifier  = stabilize_curve.interpolate(t)
+	var stabDirection = -1*sign(rotation_degrees) if abs(rotation_degrees)<= StabilizeDownAngleThershold else sign(rotation_degrees)
+	var stabilize_torque = stabDirection * stabilize_torqueForce * modifier
+	return stabilize_torque
+
+func ProcessInputForce (delta):
+	
+	var thrustforce = Vector2.ZERO
 	if Input.is_action_pressed("Up"):
-		finalforce  += thrust.rotated(rotation)
+		forward_input_level += forward_input_rate_up*delta
+		forward_input_level = min(forward_input_level,1)
+		reverse_input_level = 0
+		thrustforce  += thrust.rotated(rotation) * ForwardInputCurve.interpolate(forward_input_level)
 	elif Input.is_action_pressed("Down"): 
-		finalforce += reverse_thrust.rotated(rotation)
-	else:
-		finalforce += Vector2()
-	set_applied_force(finalforce)
+		reverse_input_level += reverse_input_rate_up*delta
+		reverse_input_level = min(reverse_input_level,1)
+		forward_input_level = 0
+		thrustforce += reverse_thrust.rotated(rotation) * ReverseInputCurve.interpolate (reverse_input_level)
+	else :
+		forward_input_level -= forward_input_rate_down*delta
+		forward_input_level = max (forward_input_level,0)
+		reverse_input_level -= reverse_input_rate_down*delta
+		reverse_input_level = max (reverse_input_level,0)
+	
+	return thrustforce
 	
 	
+func ProcessInputRotation (state): 
 	var rotation_dir = 0
+	var is_rotating_right = (state.angular_velocity > 0)
 	if Input.is_action_pressed("Right"):
 		rotation_dir += 1
 		shouldStabilize = false
 	if Input.is_action_pressed("Left"):
 		rotation_dir -= 1
 		shouldStabilize = false
-	
 	if rotation_dir == 0:
 		if $StabilizationTimer.is_stopped():
 			$StabilizationTimer.start(StabilizeDelaySeconds)
-		StabilizeUFO(shouldStabilize)
+		return (StabilizeUFO(shouldStabilize))
 	else:
-		set_applied_torque(rotation_dir * torque)
-
-func StabilizeUFO(isStab):
-	if (!isStab):
-		return
+		return (rotation_dir * torque)
 	
-	var t = abs(rotation_degrees/180)
-	var modifier  = stabilize_curve.interpolate(t)
-	var stabilize_torque = -1*sign(rotation_degrees) * stabilize_torqueForce * modifier
-	set_applied_torque (stabilize_torque)
+	
+	
+
+
+
 
 func _process(delta):
 	if Input.is_action_pressed("TractorBeam"):
